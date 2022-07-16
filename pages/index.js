5bd2fd5efff20238441508fbc958b61262520899
realemeralds <102229990+realemeralds@users.noreply.github.com>
2022-07-16 12:10:31 +0800

    :bug: fixes index bug

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import Header from "../components/Header";
 import Footer from "../components/IndexFooter";
-import Problems from "../components/Problems";
+import Problems from "../components/problems";
 import Canvas from "../components/Canvas";
 import Navbar from "../components/Navbar";
 import Solution from "../components/solution";
